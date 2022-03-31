87b87795c767a4a5fe1d7b02b0c4e29a30bebf52
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-03-30 23:51:39 -0400

    WIP: POC plugin opt-ins

--- default.nix
+++ default.nix
@@ -15,5 +15,42 @@ let
     })
   ]);
 
+  plugins = pkgs.runCommandNoCC "" {} ''
+    mkdir -p $out/lib/koreader/plugins
+    PLUGINS=(
+      coverbrowser
+    )
+    for p in "''${PLUGINS[@]}"; do
+      cp -prf ${pkgs.koreader.src}/plugins/$p.koplugin $out/lib/koreader/plugins/$p.koplugin
+    done
+  '';
+
+  inherit (pkgs)
+    koreader
+  ;
 in
-  pkgs.koreader
+  pkgs.symlinkJoin {
+    name = "koreader-configured";
+    paths = [
+      koreader
+      plugins
+    ];
+
+    # Make the launcher scripts actual scripts.
+    # This ensures BASH_SOURCE points to this directory, which in turn
+    # is used to define KOREADER_DIR
+    postBuild = ''
+      rm -vr $out/bin
+      cp -vr ${koreader}/bin $out/bin
+      chmod -R +w $out/bin
+
+      # Make sure a koreader directory exists so KOREADER_DIR points to the
+      # configure koreader even if no changes are configured.
+      mkdir -p $out/lib/koreader
+
+      # Make sure koreader uses the "configured" directory and not its actual derivation output...
+      sed -i -e 's;^KOREADER_DIR=.*;KOREADER_DIR='"$out/lib/koreader"';' $out/bin/.koreader-wrapped
+      substituteInPlace $out/bin/koreader \
+        --replace "${koreader}" "$out"
+    '';
+  }
