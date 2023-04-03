f43fbc0d402b70a6c7551aea9e76fbe2ab06ddde
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-04-02 20:59:28 -0400

    WIP: use "tangible" plugin expressions

--- default.nix
+++ default.nix
@@ -4,54 +4,77 @@
 
 # Break reference cycle
 let pkgs' = pkgs; in
-let
 
+let
   pkgs = pkgs'.appendOverlays([
     (final: super: {
       koreader = final.callPackage ./pkgs/koreader {
         luaPackages = final.luajitPackages;
         luarocks = final.luajitPackages.luarocks;
       };
+      koreader-plugins = pkgs.callPackage ./pkgs/koreader/plugins.nix {
+        inherit (final.koreader) src;
+      };
+    })
+    (final: super: {
+      # For dev...
+      koreader = super.koreader.overrideAttrs(_: {
+        patches = [];
+        src = builtins.fetchGit {
+          url = ./koreader;
+          submodules = true;
+        };
+      });
     })
   ]);
 
-  plugins = pkgs.runCommandNoCC "koreader-configured-plugins" {} ''
-    mkdir -p $out/lib/koreader/plugins
-    PLUGINS=(
-      coverbrowser
-      gestures
-    )
-    for p in "''${PLUGINS[@]}"; do
-      cp -prf ${pkgs.koreader.src}/plugins/$p.koplugin $out/lib/koreader/plugins/$p.koplugin
-    done
-  '';
-
-  inherit (pkgs)
-    koreader
+  my-plugins =
+    pkgs.callPackage (
+      { symlinkJoin, koreader-plugins }:
+      symlinkJoin {
+        name = "koreader-configured-plugins";
+        paths = with koreader-plugins; [
+          coverbrowser
+          gestures
+        ];
+      }
+    ) { }
   ;
 in
-  pkgs.symlinkJoin {
-    name = "koreader-configured";
-    paths = [
-      koreader
-      plugins
-    ];
+  rec {
+    inherit (pkgs) koreader;
+    koreader-libs = (koreader.override({ buildMemoizedLibs = true; }));
+    koreader-recombined = (koreader.override({ memoizedLibBuild = koreader-libs; }));
+    configured-koreader = pkgs.callPackage (
+      { symlinkJoin, koreader }:
 
-    # Make the launcher scripts actual scripts.
-    # This ensures BASH_SOURCE points to this directory, which in turn
-    # is used to define KOREADER_DIR
-    postBuild = ''
-      rm -vr $out/bin
-      cp -vr ${koreader}/bin $out/bin
-      chmod -R +w $out/bin
+      symlinkJoin {
+        name = "koreader-configured";
+        paths = [
+          koreader
+          my-plugins
+          # TODO: allow easily replacing all useful lua files rather than re-compile the whole app
+        ];
 
-      # Make sure a koreader directory exists so KOREADER_DIR points to the
-      # configure koreader even if no changes are configured.
-      mkdir -p $out/lib/koreader
+        postBuild = ''
+          # Make the launcher scripts actual scripts.
+          # This ensures BASH_SOURCE points to this directory, which in turn
+          # is used to define KOREADER_DIR
+          rm -vr $out/bin
+          cp -vr ${koreader}/bin $out/bin
+          chmod -R +w $out/bin
 
-      # Make sure koreader uses the "configured" directory and not its actual derivation output...
-      sed -i -e 's;^KOREADER_DIR=.*;KOREADER_DIR='"$out/lib/koreader"';' $out/bin/.koreader-wrapped
-      substituteInPlace $out/bin/koreader \
-        --replace "${koreader}" "$out"
-    '';
+          # Make sure a koreader directory exists so KOREADER_DIR points to the
+          # configured koreader even if no changes are configured.
+          mkdir -p $out/lib/koreader
+
+          # Make sure koreader uses the "configured" directory and not its actual derivation output...
+          sed -i -e 's;^KOREADER_DIR=.*;KOREADER_DIR='"$out/lib/koreader"';' $out/bin/.koreader-wrapped
+          substituteInPlace $out/bin/koreader \
+            --replace "${koreader}" "$out"
+        '';
+      }
+    ) {
+      koreader = koreader-recombined;
+    };
   }
